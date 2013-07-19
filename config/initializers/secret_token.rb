a2286614ca671fb54dbd9fc3f11e364b39cbdd92
Jonathan Moore <jonathan@stylehatch.co>
2013-07-19 10:47:56 -0700

    Time to start over. Learning through repetition and fresh start with Rails 4.0 vs upgrading

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'f5615ec5c2abf27dcf9e2e51e8f1757e996497ddb8a874e39ce56a1a46ec929debb849ba7dfe279cfb338a22b696ffdb9aafda1a29ecb367062c8332a20f12b4'
